6bfee8ac9651e52bdbf2e432e09115ed249e1d99
Shajidur_linux <shajidurrahmansaad10@gmail.com>
2021-01-18 22:21:20 +0600

    writting file in linux

--- writting_assist.py
+++ writting_assist.py
@@ -1,2 +1,20 @@
-print("windows")
-print("linux")
+import speech_recognition as sr
+import pyttsx3
+import time
+import pyautogui
+r = sr.Recognizer()
+with sr.Microphone() as mic:
+    print("speak anything")
+    listen = r.listen(mic)
+
+    try:
+        text = r.recognize_google(listen)
+        print("you said: ", format(text))
+        time.sleep(3)
+        pyautogui.typewrite(text)
+        friend = pyttsx3.init()
+        friend.say("hey stop speaking. i am writting this first")
+        friend.runAndWait()
+    except:
+        print("sorry cant get this")
+        
